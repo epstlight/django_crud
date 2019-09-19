6cd17201412510d74d4917278661ce90005b0f60
강희원 <heewkk@gmail.com>
2019-09-19 11:28:22 +0900

    Implement CRUD opertaion
     - Create articles application
     - Create Article Model
     - Implement CRUD operation for Article

---
+++ articles/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+    return redirect(f'/articles/{article.pk}')
+
+
+def index(request):
+    articles = Article.objects.all()
+    context = {'articles':articles}
+
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {'article': article}
+    return render(request, 'articles/detail.html', context)
+
+
+def delete(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('/articles/')
+
