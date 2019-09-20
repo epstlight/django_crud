92efd5b835415d09640e35e872a13cf269e129e0
강희원 <heewkk@gmail.com>
2019-09-20 09:14:05 +0900

    REST API
     - Use get_obeject_or_404
     - Apply REST API to project

--- articles/urls.py
+++ articles/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
 
 urlpatterns= [
     # 입력 페이지 제공
-    path('new/', views.new),
+    # path('new/', views.new, name='new'),
     # 데이터를 전달받아서 article 생성
-    path('create/', views.create),
-    path('', views.index),
-    path('<int:article_pk>/', views.detail),
-    path('delete/<int:article_pk>', views.delete),
+    path('create/', views.create, name='create'),
+    path('', views.index, name='index'),
+    path('<int:article_pk>/', views.detail, name='detail'), # detatil
+    path('delete/<int:article_pk>/', views.delete, name='delete'),
+    path('<int:article_pk>/update/', views.update, name='update'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
+
 
 # Create your views here.
 
-def new(request):
-    return render(request, 'articles/new.html')
-
+# def new(request):
+#     return render(request, 'articles/new.html')
 
 def create(request):
-    title = request.GET.get('title')
-    content = request.GET.get('content')
-    article = Article()
-    article.title = title
-    article.content = content
-    article.save()
-    return redirect(f'/articles/{article.pk}')
+    # 아니라면 POST 일경우 사용자 데이터 받아서 article 생성
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article()
+        article.title = title
+        article.content = content
+        article.save()
+        return redirect('articles:detail', article.pk)
+    #만약 GET 요청으로 들어오면 HTML 페이지 rendering
+    else:
+        return render(request, 'articles/create.html')
+        
 
 
 def index(request):
@@ -25,13 +31,28 @@ def index(request):
 
 
 def detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
     context = {'article': article}
     return render(request, 'articles/detail.html', context)
 
 
 def delete(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    article.delete()
-    return redirect('/articles/')
+    article = get_object_or_404(Article, pk=article_pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk) 
 
+
+def update(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article.title = title
+        article.content = content
+        article.save()
+        return redirect('articles:index')
+    else:
+        return render(request, 'articles/update.html', {'article': article})
